9f60f419912aa694513ba348e38cfb200e9456da
shehata <shehatagamal120@yahoo.com>
2017-10-02 17:20:36 +0200

    Initial Commit

---
+++ princeUse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  princeUse
+//
+//  Created by ShKhan on 10/2/17.
+//  Copyright © 2017 Approcks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ princeUse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  princeUse
+//
+//  Created by ShKhan on 10/2/17.
+//  Copyright © 2017 Approcks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
